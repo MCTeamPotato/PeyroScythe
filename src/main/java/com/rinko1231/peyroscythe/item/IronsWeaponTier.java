253d94deb7f4bb3e9a24754d84fa329eafded0d6
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2025-08-10 17:18:33 +0800

    Delete src/main directory

--- src/main/java/com/rinko1231/peyroscythe/item/IronsWeaponTier.java
+++
@@ -1,8 +0,0 @@
-package com.rinko1231.peyroscythe.item;
-public interface IronsWeaponTier {
-    float getAttackDamageBonus();
-
-    float getSpeed();
-
-    AttributeContainer[] getAdditionalAttributes();
-}
